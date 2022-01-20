cfceb3f1b18da2436f63cce231f462abb59cf66d
P-0443 <98080157+P-0443@users.noreply.github.com>
2022-01-20 14:49:12 +0530

    Add files via upload

---
+++ 2 largest number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+     int a,b,c;
+     printf("enter the value of a,b,c");
+     scanf("%d%d%d",&a,&b,&c);
+     if(a>b && a>c)
+     if(b>c)
+     printf("%d is the second largest number",b);
+     else
+     printf("%d is the second largest number",c);
+     if(b>a && b>c)
+     if(a>c)
+     printf("d is the second largest number",a);
+     else
+     printf("5d is the second largest number",c);
+     if(c>a && c>b)
+     if(b>a)
+     printf("%d is the second largest number",b);
+     else
+     printf("%d is the second largest number",a);
+     }
---
+++ Ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i;
+    for(int i=0;i<=255;i++)
+       {
+          printf("\n the ascii value of %c is %d",i,i);
+        }
+          return 0;
+}
